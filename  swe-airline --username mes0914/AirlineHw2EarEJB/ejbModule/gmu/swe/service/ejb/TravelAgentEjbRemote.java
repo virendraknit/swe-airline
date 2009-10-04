998a61646730d3d3040061e7e5ca0ce26a5e88c4
mes0914 <mes0914@4d69037c-a53a-11de-9a17-712dd7b31d32>
2009-10-04 02:13:59 +0000

    Initial import.

---
+++  swe-airline --username mes0914/AirlineHw2EarEJB/ejbModule/gmu/swe/service/ejb/TravelAgentEjbRemote.java
@@ -0,0 +1,55 @@
+package gmu.swe.service.ejb;
+
+import gmu.swe.domain.Flight;
+import gmu.swe.domain.Reservation;
+import gmu.swe.domain.SearchFilters;
+import gmu.swe.exception.DataAccessException;
+import gmu.swe.exception.ValidationException;
+
+import java.util.Collection;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface TravelAgentEjbRemote {
+
+	/**
+	 * Searches for Flights based on the provided searchFilters.
+	 * 
+	 * @param searchFilters
+	 *            Filters to used when searching for flights.
+	 * @return Collection of Flights that match the provided searchFilters
+	 * @throws ValidationException
+	 *             Thrown if there are validation errors with the provided
+	 *             searchFilters
+	 * @throws DataAccessException
+	 *             Thrown if there is an error when searching for Flights.
+	 */
+	public Collection<Flight> search(SearchFilters searchFilters) throws ValidationException, DataAccessException;
+
+	/**
+	 * Creates a reservation for on provided flight number, flightId, for the
+	 * provided number of seats, numSeats.
+	 * 
+	 * @param flightId
+	 *            Flight number the reservation should be created for.
+	 * @param numSeats
+	 *            The number of seats the reservation is for.
+	 * @return The Reservation created.
+	 * @throws ValidationException
+	 *             Thrown if there are validation errors with the provided
+	 *             information.
+	 * @throws DataAccessException
+	 *             Thrown if there is an error when creating the reservation.
+	 */
+	public Reservation createReservation(int flightId, int numSeats) throws ValidationException, DataAccessException;
+	
+	/**
+	 * Returns all airports that are in the system.
+	 * 
+	 * @return All the airports.
+	 * @throws DataAccessException
+	 *             Thrown if there is an error when retrieving the airports.
+	 */
+	public Collection<String> getAllAirports() throws DataAccessException;
+}
---
+++  swe-airline --username mes0914/AirlineHw2EarEJB/ejbModule/gmu/swe/util/NumberUtils.java
@@ -0,0 +1,44 @@
+package gmu.swe.util;
+
+public class NumberUtils {
+	/**
+	 * Utility method that determines if the provided string is a whole number.
+	 * Returns true if it is, false if it isn't (this could be because it is
+	 * text or a floating number.)
+	 * 
+	 * @param sNumber
+	 *            The String to check
+	 * @return True of sNumber is a whole number, otherwise returns false.
+	 */
+	public static boolean isWholeNumber(String sNumber) {
+		try {
+			// Test if the value is actually a number
+			new Integer(sNumber);
+			return true;
+		} catch (NumberFormatException e) {
+			// Ignore, this means it isn't a whole number.
+		} catch (NullPointerException e){
+			// Ignore, this means it isn't a whole number.
+		}
+		return false;
+	}
+	
+	/**
+	 * Utility method to determine if the provided currencyAmount is a
+	 * {@link Double}. Returns true if it is, false if it isn't.
+	 * 
+	 * @param currencyAmount
+	 *            Value to check.
+	 * @return True if currenctyAmount is a {@link Double}, false if it isn't.
+	 */
+	public static boolean isValidCurrency(String currencyAmount) {
+		try {
+			// Test if the value is actually a number
+			new Double(currencyAmount);
+			return true;
+		} catch (NumberFormatException e) {
+			// Ignore, this meants it isn't a double.
+		}
+		return false;
+	}
+}
